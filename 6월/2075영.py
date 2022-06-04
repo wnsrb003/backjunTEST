b0c470842d442f5f2f587d0ddf3c6491640ea3f9
youngji <youngji.jang804@gmail.com>
2022-06-04 12:00:34 +0900

    감샇

---
+++ 6월/2075영.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+q = []
+for _ in range(n) :
+    a = list(map(int,sys.stdin.readline().split()))
+    for i in a :
+        heapq.heappush(q,i)
+        if len(q) > n :
+            heapq.heappop(q)
+
+# q.sort()
+# result = []
+# for i in q :
+    
+
+print(q[0]) 
