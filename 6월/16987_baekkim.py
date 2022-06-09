28a490490dcc1fd703504e02f3d9fe9524bf4931
박준규 <wnsrb003@naver.com>
2022-06-09 11:29:11 +0900

    baekkim

--- 6월/16987_baekkim.py
+++ 6월/16987_baekkim.py
@@ -18,32 +18,21 @@ for _ in range(n):
                     breakEggs += 1        
             answer = max(answer, breakEggs) 
             return
-        # 자기가 깨져있는 경우 다음 계란으로    
-        if eggs[nowIndex][S] <= 0:        
+        
+	if eggs[nowIndex][S] <= 0:        
             crash(nowIndex + 1)        
             return        
-        # 자기말고 다 깨져있는 상황인 경우    
-        isAllBroken = True    
-        for targetIndex in range(n):        
-            if targetIndex == nowIndex: 
-                continue        
-            if eggs[targetIndex][S] > 0:            
-                isAllBroken = False            
-                break    
-        if isAllBroken:        
-            answer = max(answer, n - 1)        
-            return     
-        # 때려보기    
-        for targetIndex in range(n):        
+        
+	for targetIndex in range(n):        
             if targetIndex == nowIndex: 
                 continue        
             if eggs[targetIndex][S] <= 0: 
                 continue        
-            # 때리기        
+                    
             eggs[nowIndex][S] -= eggs[targetIndex][W]        
             eggs[targetIndex][S] -= eggs[nowIndex][W]        
             crash(nowIndex + 1)        
-            # 복구        
+                    
             eggs[nowIndex][S] += eggs[targetIndex][W]        
             eggs[targetIndex][S] += eggs[nowIndex][W]        
             
