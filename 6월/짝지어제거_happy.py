b815f66c8b5b1886a0c1a27f0605ef13a08128f9
박준규 <wnsrb003@naver.com>
2022-06-11 10:29:18 +0900

    jg

---
+++ 6월/짝지어제거_happy.py
@@ -0,0 +1,10 @@
+def solution(s):
+    stack = []
+    for i in range(len(s)):
+        if stack and stack[-1] == s[i]:
+            stack.pop()
+        else:
+            stack.append(s[i])
+    if stack:
+        return 0
+    return 1
