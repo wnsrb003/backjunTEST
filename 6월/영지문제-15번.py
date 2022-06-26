3583efa7fca90399c8468347820e920440dcfd96
youngji <youngji.jang804@gmail.com>
2022-06-27 00:50:35 +0900

    h

--- 6월/영지문제-15번.py
+++ 6월/영지문제-15번.py
@@ -3,4 +3,22 @@ import sys
 n,m = map(int,sys.stdin.readline().split())
 time = list(map(int,sys.stdin.readline().split()))
 
-sum
+start = max(time)
+end = sum(time) # 최대 크기
+result = 0
+while start <= end :
+    mid = (start+end)//2
+    count = 1
+    sum = time[0]
+    for i in range(1,n):
+        if mid < sum + time[i] :
+            sum = time[i]
+            count += 1
+        else : sum += time[i]
+    if count > m :
+        start = mid +1
+    else : 
+        result = mid
+        end = mid - 1
+        
+print(result)
--- 6월/영지문제-16번.py
+++ 6월/영지문제-16번.py
@@ -1,2 +1,27 @@
 import sys
 
+k,n = map(int,sys.stdin.readline().split())
+lensun = []
+for _ in range(k) :
+    a = int(sys.stdin.readline())
+    lensun.append(a)
+
+start = 1
+end = max(lensun)
+result = 0
+while start <= end : 
+    count = 0
+    mid = (start+end) // 2
+    for i in lensun :
+        while i >= mid :
+            i = i - mid
+            count += 1
+            if count > n : break
+        if count > n : break
+    if count < n :
+        end = mid - 1
+    else :
+        result = mid #최대니까
+        start = mid + 1
+
+print(result)
